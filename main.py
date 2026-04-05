687e92cf6437ff2dc5ef2bf510672565c747c223
ordinalsh <aleixcotaina@gmail.com>
2026-04-06 00:54:41 +0200

    modificar endpoints y modificar track.html

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from source.config import Config
 from routers import frontend
+from routers import endpoints
 import os
 import uvicorn
 
@@ -10,11 +11,13 @@ app = FastAPI()
 
 # Incluir routers
 app.include_router(frontend.router)
+app.include_router(endpoints.router)
 
 # Inicializador --------------------------------------------------
 if __name__  == "__main__":
     uvicorn.run("main:app",
         host=server_config.get_key("host", "0.0.0.0"),
         port=server_config.get_key("port", 80),
-        reload=server_config.get_key("reload", True)
+        reload=server_config.get_key("reload", True),
+        workers=server_config.get_key("workers", 4)
     )
--- routers/endpoints.py
+++ routers/endpoints.py
@@ -1,2 +1,33 @@
 from fastapi import APIRouter, Request, Depends
-from source.dependencies import get_db
+from fastapi.responses import JSONResponse
+from source.dependencies import get_db, Database
+
+router = APIRouter(prefix="/api", tags=["endpoint"])
+
+@router.get("/get_project/{track_id}")
+async def get_project_endpoint(track_id: str, db: Database = Depends(get_db)):
+    """
+    Este endpoint devuelve:
+    {
+        "id": int,
+        "name": str,
+        "project_author_id": str,
+        "project_track_id": str,
+        "project_description": str,
+        "project_status": str,
+        "project_priority": int,
+        "milestones": Dict[str, bool] | None,
+        "overall_progress": int,
+        "last_update": datetime
+    }
+
+    o codigo 404 si no existe.
+    """
+    tracker = db.get_project_by_track_id(track_id)
+    if not tracker:
+        return JSONResponse({
+            "message": "No project found"
+        }, status_code=404)
+    return JSONResponse({
+        "track": tracker
+    })
